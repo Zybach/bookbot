fec9f3f0d4d6f8fa358a5ed96a3a808a399b8405
Zybach <rolandmatthes_ffxi@live.de>
2024-08-27 21:36:24 +0200

    wrote main function, used with and open and read, elegant way

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+def main():
+
+    """using with methode and open function to open a filepath in quotes, save it as f, and using the .read methode to read the file and assign it to
+    file_content """
+
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+def get_book_text(path):
+
+    
+    with open(path) as f:
+        return f.read()
+        #print(file_content)
+
+
+
+main()
